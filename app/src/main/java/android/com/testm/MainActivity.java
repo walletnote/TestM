a7aede3a3b19558d8ae79952cccda7af391c9d13
ramu <babu.ramu111@gmail.com>
2018-07-07 19:34:49 +0530

    testi

--- app/src/main/java/android/com/testm/MainActivity.java
+++ app/src/main/java/android/com/testm/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
         // this is kalyan branch
 
-        // kalyan new comment // new conflict
+        // kalyan new comment // new conflict // hari conflict
+
     }
 }
